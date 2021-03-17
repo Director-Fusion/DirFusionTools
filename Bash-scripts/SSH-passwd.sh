5177cd8c97cd436370274c018cfcbcaade658cf2
Director-Fusion <42795560+Director-Fusion@users.noreply.github.com>
2021-03-17 14:15:03 -0600

    Update SSH-passwd.sh

--- Bash-scripts/SSH-passwd.sh
+++ Bash-scripts/SSH-passwd.sh
@@ -13,18 +13,20 @@
 IPs="/home/dirfusion/Desktop/IPs.txt"
 
 #User Name
-echo -n "What is your user name?: "
-read user
+read -p "What is your user name?: " user
+echo $user
 
 #First you enter in your current password.
 #Second Enter your new password.
 
 #Takes input for current password.
-echo -n "Enter your current-password: "
-read current
+read -p "Enter your current-password: " current
+echo $current
 
 #Verifies typed password
-echo "Your current password is: $current, is this correct?"
+echo "Your current password is: "
+echo $current
+echo " is this correct?"
 read -p "Press Enter to continue or Ctrl+C to cancel"
 
 #Takes input for new password
@@ -32,14 +34,16 @@ echo -n "Enter your new-password: "
 read new
 
 #Verifies the new password
-echo "Your new password is: $new, is this correct?"
+echo "Your new password is: "
+echo $new
+echo " is this correct?"
 read -p "Press Enter to continue or Ctrl+C to cancel..."
 
 for IP in $(IPs)
 do
-  ssh -t $user@$IP passwd
-  send "$current\r"
-  send "$new\r"
-  send "$new\r"
-  send "exit\r"
+  ssh $IP -l $user -t passwd
+  send $current
+  send $new
+  send $new
+  exit
 done
