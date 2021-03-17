7910856bd1b62824a4a28df43d6f036cc7677bca
Director-Fusion <42795560+Director-Fusion@users.noreply.github.com>
2021-03-17 08:51:31 -0600

    Create SSH-passwd.sh

---
+++ Bash-scripts/SSH-passwd.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#WARNING - There is no logic flow to prompt users to ensure the passwords typed are correct. Type carefully.
+
+# This is to reset your password on devices via ssh for a linux environment. This is to be coupled with a IPs.txt to take input from
+# so the password can be reset. I have a 30 day password reset requirement. I run this on the last monday of each month. I keep the list of 
+# IP addresses in the same directory as my script. Ctrl+C or Ctrl+Z to cancel. 
+
+#IP list.
+IPs="/home/dirfusion/Desktop/IPs.txt"
+
+#User Name
+echo -n "What is your user ame?: "
+read user
+
+#First you enter in your current password.
+#Second Enter your new password.
+
+#Takes input for current password.
+echo -n "Enter your current-password: "
+read current
+
+#Verifys typed password
+echo "Your current password is: $current, is this correct?
+read -p "Press Enter to continue or Ctrl+C to cancel
+
+#Takes input for new password
+echo -n "Enter your new-password: "
+read new
+
+#Verifies New password
+echo "Your current password is: $new, is this correct?
+read -p "Press Enter to continue or Ctrl+C to cancel
+
+for IP in $(IPs)
+do
+  ssh -t $user@$IP passwd
+  send "$current\r"
+  send "$new\r"
+  send "$new\r"
+  send "exit\r"
+done
