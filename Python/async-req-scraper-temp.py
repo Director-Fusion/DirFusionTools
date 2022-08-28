4cf012e7742f06300f9eb08937dd1a09341c1cdc
Director-Fusion <cory.kelleraf@gmail.com>
2022-08-28 13:14:51 -0600

    Async API requests 2

--- Python/async-req-scraper-temp.py
+++ Python/async-req-scraper-temp.py
@@ -6,8 +6,8 @@ import time
 
 async def get_items(client, url):
     resp = await client.get(url)
-    name = resp.json()['name']
-    return print(name)
+    results.append(resp.json()['name'])
+    return
 
 async def main():
     async with httpx.AsyncClient() as client:
@@ -15,7 +15,9 @@ async def main():
         for req_id in range(1,151):
             url = f"https://pokeapi.co/api/v2/pokemon/{req_id}"
             tasks.append(asyncio.create_task(get_items(client, url)))
-        await asyncio.gather(*tasks)    
+        await asyncio.gather(*tasks)
+results = []        
 start = time.time()          
 asyncio.run(main())
 print(f"Requests: {time.time() -  start} seconds.")  
+print(results)
